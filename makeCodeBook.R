6b289da731f85b7eb58666dbd727f38c7570da1c
Chris <chris.ballenger@gmail.com>
2018-06-27 18:40:11 -0500

    Initial upload

---
+++ makeCodeBook.R
@@ -0,0 +1,18 @@
+setwd("/Users/christopherballenger/Documents/Data Science/MSDS 6306/Projects/CaseStudy1")
+
+brew <- read.csv(file = "breweries.csv", header = TRUE, sep = ",", strip.white = TRUE)
+beer <- read.csv(file = "beers.csv", header = TRUE, sep = ",", strip.white = TRUE)
+brewbeer <- merge(brew, beer, by.x = "Brew_ID", by.y="Brewery_id")
+names(brewbeer) <- c('Beer_ID','Brewery','City','State','Beer','Beer_ID','ABV','IBU','Style','Ounces')
+
+# Provide descriptions of each variable
+attr(brewbeer$ABV, "shortDescription") <-
+    "Alcohol by Volume, measurement is not in percentage"
+
+attr(brewbeer$IBU, "shortDescription") <-
+    "International Bittering Units"
+
+attr(brewbeer$Ounces, "shortDescription") <- 
+    "Recommended Serving Size"
+
+makeCodebook(brewbeer, replace=TRUE)
